b7e3a27faa3463747e613c8f9307eebbb3ab03b0
richardtrimble <richardtrimble@hotmail.com>
2013-12-29 20:41:09 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -57,23 +57,23 @@ var app = {
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
         scanner.scan( function (result) { 
-
+            /*
             alert("We got a barcode\n" + 
             "Result: " + result.text + "\n" + 
             "Format: " + result.format + "\n" + 
             "Cancelled: " + result.cancelled);  
-
+            */
            console.log("Scanner result: \n" +
                 "text: " + result.text + "\n" +
                 "format: " + result.format + "\n" +
                 "cancelled: " + result.cancelled + "\n");
             document.getElementById("info").innerHTML = result.text;
             console.log(result);
-            /*
-            if (args.format == "QR_CODE") {
-                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+            
+            if (result.format == "EAN_13" || result.format == "EAN_8") {
+                window.plugins.childBrowser.showWebPage('http://www.getbook.co.uk/search?q='+result.text, {});
             }
-            */
+            
 
         }, function (error) { 
             console.log("Scanning failed: ", error); 
